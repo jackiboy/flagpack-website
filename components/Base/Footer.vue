a3676b1e4705cfb315b29e5376ccce2d77f505b6
Jack <jack@jacks.site>
2018-11-19 16:13:24 +0100

    added about footer

---
+++ components/Base/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+        <div class="footer--link">
+          project by&nbsp;
+          <a href="https://jacks.site" target="_blank">
+            <img src="/jack.svg" alt="Jack Marsh. Freelance Full Stack Developer">
+          </a>
+        </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "base-footer",
+};
+</script>
--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -1,33 +1,36 @@
 <template>
-    <div class="body" style="background-image: url('/wave.svg')">
-      <transition name="details">
-        <modal-details v-if="details"></modal-details>
-      </transition>
-      <base-nav></base-nav>
-      <base-header></base-header>
-      <base-tabs></base-tabs>
-      <main class="container">
-        <section class="countries__table" v-if="results.length > 0"> 
-          <country
-            v-for="(country, index) in results" 
-            :key="index"
-            :country="country"
-            :code="country.code|lowercase"
-            :continent="country.continent"
-            v-if="country.name !== 'Antarctica'"
-          ></country>
-        </section>
-        <section class="countries__table" v-else> 
-          <country
-            v-for="(country, index) in countries" 
-            :key="index"
-            :country="country"
-            :code="country.code|lowercase"
-            :continent="country.continent"
-            v-if="country.name !== 'Antarctica'"
-          ></country>
-        </section>
-      </main>
+    <div>
+      <div class="body" style="background-image: url('/wave.svg')">
+        <transition name="details">
+          <modal-details v-if="details"></modal-details>
+        </transition>
+        <base-nav></base-nav>
+        <base-header></base-header>
+        <base-tabs></base-tabs>
+        <main class="container">
+          <section class="countries__table" v-if="results.length > 0"> 
+            <country
+              v-for="(country, index) in results" 
+              :key="index"
+              :country="country"
+              :code="country.code|lowercase"
+              :continent="country.continent"
+              v-if="country.name !== 'Antarctica'"
+            ></country>
+          </section>
+          <section class="countries__table" v-else> 
+            <country
+              v-for="(country, index) in countries" 
+              :key="index"
+              :country="country"
+              :code="country.code|lowercase"
+              :continent="country.continent"
+              v-if="country.name !== 'Antarctica'"
+            ></country>
+          </section>
+        </main>
+      </div>
+      <base-footer></base-footer>
     </div>
 </template>
 
@@ -36,6 +39,7 @@ import noScroll from 'no-scroll';
 import Nav from '~/components/Base/Nav';
 import Header from '~/components/Base/Header/Header';
 import Tabs from '~/components/Base/Tabs';
+import Footer from '~/components/Base/Footer';
 import Country from '~/components/Country';
 import Details from '~/components/Details';
 
@@ -60,6 +64,7 @@ export default {
     "base-nav": Nav, 
     "base-header": Header,
     "base-tabs": Tabs,
+    "base-footer": Footer,
     "country": Country,
     "modal-details": Details
   },
